6fe1212c37a8d85a4e0d212672b3a2ef38dfd619
araobp <araobp@users.noreply.github.com>
2025-11-08 13:51:39 +0900

    feat(app): Add tabbed interface to main page
    Adds a tabbed interface to the main page with "Plan" and "Simulation" tabs.
    
    The "Simulation" tab now embeds the Godot scene in an iframe, allowing for a more integrated experience.
    
    Bootstrap is added to the project for styling purposes.

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container mt-3" style="height: 100vh;">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button
+                class="nav-link active"
+                id="plan-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#plan-tab-pane"
+                type="button"
+                role="tab"
+                aria-controls="plan-tab-pane"
+                aria-selected="true">Plan</button
+            >
+        </li>
+        <li class="nav-item" role="presentation">
+            <button
+                class="nav-link"
+                id="simulation-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#simulation-tab-pane"
+                type="button"
+                role="tab"
+                aria-controls="simulation-tab-pane"
+                aria-selected="false">Simulation</button
+            >
+        </li>
+    </ul>
 
-<a href="/godot.html" target="_blank">Godot Scene</a>
+    <div class="tab-content h-100" id="myTabContent">
+        <div
+            class="tab-pane fade show active"
+            id="plan-tab-pane"
+            role="tabpanel"
+            aria-labelledby="plan-tab"
+            tabindex="0"
+        >
+            ...
+        </div>
+        <div
+            class="tab-pane one"
+            style="height: 90%;"
+            id="simulation-tab-pane"
+            role="tabpanel"
+            aria-labelledby="simulation-tab"
+            tabindex="0"
+        >
+            <iframe style="width: 100%; height: 100%;" src="/godot.html"> </iframe>
+        </div>
+    </div>
+</div>
