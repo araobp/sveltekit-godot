a64528b35348be9f01fc20b51fbd38c1d622a1ae
araobp <araobp@users.noreply.github.com>
2025-11-16 09:34:44 +0900

    update

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,16 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-      pages: '../docs/live',
-			fallback: '404.html'
-		}),
+		adapter: adapter(
+			{
+				pages: "../docs/live",
+				assets: "../docs/live",
+			}
+		),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+		},
+	},
 };
 
 export default config;
